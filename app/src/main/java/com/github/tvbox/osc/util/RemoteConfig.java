3578de4b9a6a7c2eeb7d1172113cb997667cfad6
xihi <xuhui0607@gmail.com>
2023-08-23 21:54:58 +0800

    qq

--- app/src/main/java/com/github/tvbox/osc/util/RemoteConfig.java
+++ app/src/main/java/com/github/tvbox/osc/util/RemoteConfig.java
@@ -27,9 +27,9 @@ public class RemoteConfig {
     public static void Init(Context mContext){
         RemoteConfig.mContext = mContext;
         if (ToolUtils.isApkInDebug(mContext)){
-            remoteUrl = "https://gitee.com/xisohi/XHYSosc/raw/master/XHYSTVupdate.json";
+            remoteUrl = "https://gitee.com/xisohi/XHYSosc/raw/master/XHYSTVupdate.txt";
         }else{
-            remoteUrl = "https://gitee.com/xisohi/XHYSosc/raw/master/XHYSTVupdate.json";
+            remoteUrl = "https://gitee.com/xisohi/XHYSosc/raw/master/XHYSTVupdate.ini";
         }
         LOG.e("RemoteConfig",
                 ToolUtils.isApkInDebug(mContext) ? "当前处于【调试】模式":"当前处于【正式】模式",
