6cf0c08eae79deb4f0c2b0f1ba29c1e828ba154d
xihi <xuhui0607@gmail.com>
2023-11-10 16:05:19 +0800

    Update HomeActivity.java

--- app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
@@ -482,7 +482,7 @@ public class HomeActivity extends BaseActivity {
             super.onBackPressed();
         } else {
             mExitTime = System.currentTimeMillis();
-            Toast.makeText(mContext, "再按一次返回键退出应用", Toast.LENGTH_SHORT).show();
+            Toast.makeText(mContext, "再按一次返回键退出应用", Toast.LENGTH_SHORT).show();            
         }
     }
 
