089cc9534197d9f54d8a7e060b91712ff8bbcfeb
xihi <xuhui0607@gmail.com>
2025-03-17 19:28:37 +0800

    添加内置直播地址

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -167,7 +167,7 @@ public class ApiConfig {
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
         String apiUrl = Hawk.get(HawkConfig.API_URL, "http://lcjly.cn/tvbox/XHYSyuan.json");
         //独立加载直播配置
-        String liveApiUrl = Hawk.get(HawkConfig.LIVE_API_URL, "https://xhys.lcjly.cn/live.txt");
+        String liveApiUrl = Hawk.get(HawkConfig.LIVE_API_URL, "http://lcjly.cn/tvbox/XHYSyuan.json");
         String liveApiConfigUrl=configUrl(liveApiUrl);
         if(!liveApiUrl.isEmpty() && !liveApiUrl.equals(apiUrl)){
             if(liveApiUrl.contains(".txt") || liveApiUrl.contains(".m3u") || liveApiUrl.contains("=txt") || liveApiUrl.contains("=m3u")){
