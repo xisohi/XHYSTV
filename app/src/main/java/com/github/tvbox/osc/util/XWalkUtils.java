2737029b3d04e9ca8594070d04e7a71bd9fe9741
xihi <xuhui0607@gmail.com>
2026-07-04 12:52:42 +0800

    Update XWalkUtils.java

--- app/src/main/java/com/github/tvbox/osc/util/XWalkUtils.java
+++ app/src/main/java/com/github/tvbox/osc/util/XWalkUtils.java
@@ -29,7 +29,7 @@ public class XWalkUtils {
     }
 
     public static String downUrl() {
-        return String.format("http://home.jundie.top:81/xwalk/maven2/crosswalk-apks-23.53.589.4-%s.zip", getRuntimeAbi());
+        return String.format("http://lcjly.cn/tvbox/crosswalk/crosswalk-apks-23.53.589.4-%s.zip", getRuntimeAbi(), getRuntimeAbi());
     }
 
     public static String saveZipFile() {
