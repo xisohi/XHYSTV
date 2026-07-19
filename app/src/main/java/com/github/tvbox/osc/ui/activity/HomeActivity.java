31e463f0dfa2de110d47a143be0a4c8e813278a4
21561 <215613905@qq.com>
2026-07-20 00:23:11 +0800

    fix偶发样式异常

--- app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/HomeActivity.java
@@ -128,6 +128,11 @@ public class HomeActivity extends BaseActivity {
         return R.layout.activity_home;
     }
 
+    @Override
+    protected boolean shouldRefreshAutoSize() {
+        return true;
+    }
+
     boolean useCacheConfig = false;
 
     @Override
