b760d80584a795d58d2a5770bf165bf45057ca08
于俊 <215613905@qq.com>
2026-08-17 17:45:46 +0800

    去广告增强

--- app/src/main/java/com/github/tvbox/osc/base/App.java
+++ app/src/main/java/com/github/tvbox/osc/base/App.java
@@ -65,6 +65,7 @@ public class App extends MultiDexApplication {
         // Hawk
         Hawk.init(this).build();
         Hawk.put(HawkConfig.DEBUG_OPEN, false);
+        Hawk.put(HawkConfig.PLAYER_IS_LIVE, false);
         if (!Hawk.contains(HawkConfig.PLAY_TYPE)) {
             Hawk.put(HawkConfig.PLAY_TYPE, 1);
         }
